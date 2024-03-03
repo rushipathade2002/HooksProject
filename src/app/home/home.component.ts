ab53815c2851d22f8070bdb282db5086716b2d40
rushipathade96 <rushikeshpathade8698@gmail.com>
2024-03-03 19:11:33 +0530

    write Hooks

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  interval:any;
     constructor(){
       console.log("home Constructor");
       
     }
+  ngOnDestroy() {
+    console.log("Bye Bye...");
+    clearInterval(this.interval);
+
+  }
+  
+  ngOnInit() {
+    var i=0;
+    this.interval=setInterval(()=>{
+      console.log(i);
+      i++;
+    }, 1000);
+    console.log("Home Component NgOnInit");
+    
+  }
 }
